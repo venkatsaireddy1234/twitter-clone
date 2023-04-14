57f7507edc7e22666eda5bffdfd3cfebcc5a9414
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-14 12:26:58 +0530

    SideBar UI is done

---
+++ components/SideBarMenuItem.js
@@ -0,0 +1,8 @@
+export default function SideBarMenuItem({text, Icon, active}) {
+  return (
+    <div  className="hoverEffect flex items-center justify-center xl:justify-start space-x-3 text-lg">
+        <Icon className= "h-7"/>
+        <span className={`${active && "font-bold"} hidden xl:inline`}>{text}</span>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
+import SideBar from "@/components/SideBar";
+import Head from "next/head";
+
+
 
 export default function Home() {
   return (
-   <div>Twitter clone</div>
-  )
+    <div>
+      <Head>
+        <title>Twitter clone</title>
+      </Head>
+      <main className="flex min-h-screen max-w-7xl mx-auto">
+      <SideBar />
+      </main>
+    </div>
+  );
 }
