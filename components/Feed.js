3b6fb30c99fc3eacb7f076e15518ecae01cd304c
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-14 16:45:57 +0530

    PostFeed

--- components/Feed.js
+++ components/Feed.js
@@ -1,16 +1,52 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "venkat",
+      userName: "vnktsai",
+      timeStamp: "2 hours ago",
+      postImage:
+        "https://plus.unsplash.com/premium_photo-1669741178222-004a82192170?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      userImage:
+        "https://pbs.twimg.com/profile_images/705952285965291521/T19JWacu_400x400.jpg",
+      text: "wow",
+    },
+    {
+      id: "2",
+      name: "sai",
+      userName: "sai",
+      timeStamp: "3 hours ago",
+      postImage:
+        "https://images.unsplash.com/photo-1681454450359-023f4f1b5466?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1325&q=80",
+      userImage:
+        "https://pbs.twimg.com/profile_images/705952285965291521/T19JWacu_400x400.jpg",
+      text: "Jet",
+    },
+  ];
   return (
     <div className="xl:ml-[370px]  border-l border-r xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex sticky px-2 py-2 z-50 border-b bg-white border-gray-200">
         <div className="text-lg sm:text-xl font-bold cursor-pointer">Home</div>
         <div className="hoverEffect flex items-center justify-center ml-auto w-9 h-9 px-0">
-          <SparklesIcon className="  h-5 " />
+          <SparklesIcon className="h-5 " />
         </div>
       </div>
       <Input />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          userName={post.userName}
+          name={post.name}
+          postImage={post.postImage}
+          userImage={post.userImage}
+          text={post.text}
+          timeStamp={post.timeStamp}
+        />
+      ))}
     </div>
   );
 }
---
+++ components/Post.js
@@ -0,0 +1,49 @@
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
+import { ChatIcon } from "@heroicons/react/outline";
+import { TrashIcon } from "@heroicons/react/outline";
+import { HeartIcon } from "@heroicons/react/outline";
+import { ShareIcon } from "@heroicons/react/outline";
+import { ChartBarIcon } from "@heroicons/react/outline";
+
+export default function Post({
+  key,
+  userName,
+  userImage,
+  postImage,
+  text,
+  timeStamp,
+  name,
+}) {
+  return (
+    <div className="flex border-b border-gray-200 py-3 cursor-pointer ">
+      {/* {user-image} */}
+      <img className = "h-11 w-11 rounded-full ml-2 mr-4" src={userImage} alt="userImage" />
+      <div>
+        {/* {Header} */}
+        <div className="flex items-center justify-between">
+          {/* {user-info} */}
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] hover:underline sm:text-[16px]">{name}</h4>
+            <span className="text-sm sm:text-[15px]">@{userName} -</span>
+            <span className="text-sm sm:text-[15px] hover:underline">{timeStamp}</span>
+          </div>
+          {/* {icon} */}
+          <DotsHorizontalIcon className="h-10 hoverEffect hover:bg-sky-200 hover:text-500" />
+        </div>
+          <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">{text}</p>
+        <div>
+          {/* {post-image} */}
+          <img className="rounded-2xl mr-2" src={postImage} alt="postImage" />
+          {/* {icons} */}
+          <div className="flex justify-between  text-gray-500 p-2">
+            <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+            <TrashIcon className="h-9 w-9  hoverEffect p-2  hover:text-red-600 hover:bg-red-100" />
+            <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+            <ShareIcon className="h-9 w-9 hoverEffect p-2  hover:text-sky-500 hover:bg-blue-100" />
+            <ChartBarIcon className="h-9 w-9 hoverEffect p-2  hover:text-sky-500 hover:bg-sky-100" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
