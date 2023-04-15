b964d75ca300d17be5d07af75d3b81c32e65442a
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-15 10:59:31 +0530

    Created a random users Sections to follow to a widgets components

--- components/Widgets.js
+++ components/Widgets.js
@@ -2,8 +2,10 @@ import { SearchIcon } from "@heroicons/react/outline";
 import News from "./News";
 import { useState } from "react";
 
-export default function Widgets({ newsResults }) {
+export default function Widgets({ newsResults, randomUsersResults }) {
   const [articleNum, setArticleNum] = useState(3);
+  const [userNum, setUserNum] = useState(3);
+
 
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
@@ -29,7 +31,36 @@ export default function Widgets({ newsResults }) {
           Show more
         </button>
       </div>
+      <div className="text-gray-700 space-y-3 bg-gray-100  pt-2 rounded-xl w-[90%] xl:w-[75%] sticky top-30">
+        <h4 className="font-bold text-xl px-4">Who to Follow</h4>
+        {randomUsersResults.slice(0,userNum).map((user) => (
+          <div
+            key={user.login.username}
+            className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200"
+          >
+            <img
+              src={user.picture.thumbnail}
+              className="rounded-full"
+              width="40"
+            ></img>
+            <div className="truncate ml-4 leading-5">
+              <h4 className="font-bold hover:underline text-[14px] truncate">
+                {" "}
+                {user.login.username}
+              </h4>
+              <h5 className=" text-[13px] text-gray-500 truncate">
+                {user.name.first + " " + user.name.last}
+              </h5>
+            </div>
+            <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold ">
+              Follow
+            </button>
+          </div>
+        ))}
+        <button className="text-blue-300 pl-4 pb-3 hover:text-blue-400" onClick={()=>{setUserNum(userNum+3)}}>
+          Show more
+        </button>
+      </div>
     </div>
   );
 }
-
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import SideBar from "@/components/SideBar";
 import Widgets from "@/components/Widgets";
 import Head from "next/head";
 
-export default function Home({ newsResults }) {
+export default function Home({ newsResults,randomUsersResults }) {
   return (
     <div>
       <Head>
@@ -12,7 +12,7 @@ export default function Home({ newsResults }) {
       <main className="flex min-h-screen mx-auto">
         <SideBar />
         <Feed />
-        <Widgets newsResults={newsResults.articles} />
+        <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results}/>
       </main>
     </div>
   );
@@ -24,9 +24,15 @@ export async function getServerSideProps() {
   const newsResults = await fetch(
     "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
   ).then((res) => res.json());
+
+  //who to follow section
+  const randomUsersResults = await fetch(
+    "https://randomuser.me/api/?results=50&inc=name,login,picture"
+  ).then((res) => res.json());
   return {
     props: {
       newsResults,
+      randomUsersResults
     },
   };
 }
