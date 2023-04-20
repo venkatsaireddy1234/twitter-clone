a466d254676a3696a70a3569d271d02a919b8634
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-20 17:25:35 +0530

    React-hook-form for the Edit-profile

--- components/EditForm.js
+++ components/EditForm.js
@@ -1,65 +1,154 @@
-import { useForm, Controller } from "react-hook-form";
+import { Controller, useController, useForm } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import { useRecoilState } from "recoil";
 import Modal from "react-modal";
 import { editFormModalState } from "@/atom/modalAtom";
+import "react-datepicker/dist/react-datepicker.css";
+import { XIcon } from "@heroicons/react/solid";
+import { PhotographIcon } from "@heroicons/react/outline";
+import { useRef } from "react";
+import { getDownloadURL, ref, uploadString } from "firebase/storage";
 
 export default function EditForm() {
   const [openEditForm, setOpenEditForm] = useRecoilState(editFormModalState);
+  const filePickerRef = useRef(null);
 
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const addBgImageToPost = async (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  };
 
-  //   const location = data.location.split(',').map(parseFloat);
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const {
+    field: { value, onChange, onBlur },
+    fieldState: { invalid },
+  } = useController({
+    control,
+    name: "dateOfBirth",
+    rules: { required: "Date of birth is required" },
+    defaultValue: "",
+  });
+
+  const onSubmit = (data) =>{
+    setOpenEditForm(false)
+  } 
+
   return (
-    <div>
+    <div className="flex flex-col">
       {openEditForm && (
-        <Modal isOpen={open} onRequestClose={() => setOpen(false)}>
+        <Modal
+          isOpen={openEditForm}
+          onRequestClose={() => setOpenEditForm(!openEditForm)}
+          className="max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-200 rounded-xl shadow-md"
+        >
           <form onSubmit={handleSubmit(onSubmit)}>
-            <DatePicker
-              name="date"
-              selected={null}
-              onChange={(date) => setValue("date", date)}
-              dateFormat="dd/MM/yyyy"
-              ref={register({
-                required: "Date is required",
-              })}
-            />
-            {errors.date && <span>{errors.date.message}</span>}
+            
+            <div className=" m-2 py-2 flex space-x-2 items-center -mb-2">
+              <div
+                onClick={() => setOpenEditForm(false)}
+                className="hoverEffect w-11 h-10 flex items-center justify-center "
+              >
+                <XIcon className="h-5" />
+              </div>
+              <h4 className="font-bold"> Edit Your Profile</h4>
+            </div>
+            <div className="flex flex-col  justify-center">
+            
+             
+              <Controller
+                name="name"
+                control={control}
+                rules={{ required: "Name is required" }}
+                render={({ field }) => (
+                  <>
+                    <input
+                      {...field}
+                      type="text"
+                      placeholder="Your Name"
+                      className="rounded-xl p-2 space-x-2 m-2"
+                    />
+                  </>
+                )}
+              />
+              {errors.name && <span  className="text-red-500 ml-3 ">{errors.name.message}</span>}
 
-            <input
-              type="text"
-              name="description"
-              ref={register({
-                required: "Description is required",
-              })}
-            />
-            {errors.inputValue && <span>{errors.inputValue.message}</span>}
-            <Input
-              name="location"
-              type="text"
-              register={register({
-                required: "Location is required",
-                pattern: {
-                  value:
-                    /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/,
-                  message: "Invalid geolocation format",
-                },
-              })}
-              placeholder="Enter geolocation (latitude, longitude)"
-              error={errors.location && errors.location.message}
-            />
-            <Controller
-              name="image"
-              control={control}
-              render={({ field }) => (
-                <input
-                  type="file"
-                  onChange={(e) => field.onChange(e.target.files[0])}
+                <DatePicker
+                selected={value}
+                onChange={onChange}
+                onBlur={onBlur}
+                dateFormat="MM/dd/yyyy"
+                name="dateOfBirth"
+                className="rounded-xl p-2 space-y-2 m-2"
+                placeholderText="Birth date"
+              />
+              {errors.dateOfBirth && <span  className="text-red-500 ml-3 ">{errors.dateOfBirth.message}</span>}
+              
+
+              <Controller
+                name="location"
+                control={control}
+                rules={{ required: "Location is required" }}
+                render={({ field }) => (
+                  <>
+                    <input
+                      {...field}
+                      type="text"
+                      placeholder="Location"
+                      className="rounded-xl p-2 space-x-2 m-2"
+                    />
+                  </>
+                )}
+              />
+              {errors.location && <span  className="text-red-500 ml-3 ">{errors.location.message}</span>}
+              <Controller
+                name="bio"
+                control={control}
+                rules={{ required: "Bio is required" }}
+                render={({ field }) => (
+                  <textarea
+                    {...field}
+                    placeholder="Bio"
+                    className="rounded-xl p-2 space-x-2 m-2"
+                  />
+                )}
+              />
+
+              {errors.bio && <span className="text-red-500 ml-3 ">{errors.bio.message}</span>}
+              <div className="flex flex-col justify-center">
+              <h2 className="text-blue-500 m-3 -mb-2">
+                Back-Ground Image
+              </h2>
+              <input
+                type="file"
+                name="backgroundImage"
+                ref={register()}
+                className="rounded-xl p-2 space-x-2 m-2"
                 />
-              )}
-            />
-            <input type="submit" />
+                </div>
+            </div>
+            <div className="flex m-4 justify-between ">
+              <button
+                type="submit"
+                className="bg-blue-400 rounded-full px-4 py-1.5 text-white hover:brightness-95 shadow-md disabled:opacity-50"
+              >
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={() => setOpenEditForm(false)}
+                className="bg-red-400 rounded-full px-4 py-1.5 text-white hover:brightness-95 shadow-md disabled:opacity-50"
+              >
+                Cancel
+              </button>
+            </div>
           </form>
         </Modal>
       )}
--- components/ProfileDetails.js
+++ components/ProfileDetails.js
@@ -2,15 +2,18 @@ import { db } from "@/firebase";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import {
-  LocationMarkerIcon,CakeIcon,CalendarIcon
+  LocationMarkerIcon,
+  CakeIcon,
+  CalendarIcon,
 } from "@heroicons/react/outline";
 import { useRecoilState } from "recoil";
 import { editFormModalState } from "@/atom/modalAtom";
 import { signIn, useSession } from "next-auth/react";
+import EditForm from "./EditForm";
 
-export default function ProfileDetails({  }) {
+export default function ProfileDetails({}) {
   const [posts, setPosts] = useState([]);
-  const {data:session} = useSession();
+  const { data: session } = useSession();
   const [openEditForm, setOpenEditForm] = useRecoilState(editFormModalState);
   useEffect(
     () =>
@@ -22,59 +25,64 @@ export default function ProfileDetails({  }) {
       ),
     []
   );
-  const  userPhoto = posts.map((post)=>post.data().userImg)
-  
+  const userPhoto = posts.map((post) => post.data().userImg);
+
   return (
     <div className="flex flex-col ">
-      <div className="bg-gray-100 w-full p-20">
-      <img
-            className="h-11 w-11  mr-4  absolute"
-            src={userPhoto}
-            alt="userImage"
-            />
-            </div>
-      <div className="flex items-center justify-between">
+      <div className="bg-gray-100 w-full p-20 border border-white-500">
+        <img className="fixed" src={userPhoto} alt="userImage" />
+      </div>
+      <div className="flex items-center justify-between ">
         <div className="">
           <img
-            className="h-11 w-11 rounded-full mr-4 relative"
+            className="h-13 w-13 rounded-full ml-4 -mt-12 absolute "
             src={userPhoto[0]}
             alt="userImage"
           />
         </div>
-        <div>
-          <button 
-          onClick={()=>{
-            if(!session){
-              signIn()
-            }else{
-              setOpenEditForm(!openEditForm),console.log("hi")}}
-            }
-          className="border border-gray-200 rounded-full font-bold mr-2 p-2">Edit Profile</button>
+        <div className="mt-2">
+          <button
+            onClick={() => {
+              if (!session) {
+                signIn();
+              } else {
+                setOpenEditForm(!openEditForm);
+              }
+            }}
+            className="border border-gray-200 rounded-full font-bold mr-2 p-2 -mb-5"
+          >
+            Edit Profile
+          </button>
         </div>
       </div>
-      <div className="flex ml-3 mt-3 flex-col">
-      <h4 className="font-bold text-[20px] hover:underline sm:text-[16px]">
-              Venkat
-            </h4>
-            <span className="text-sm text-gray-500 sm:text-[13px]">
-              @venkat1234
-            </span>
-            <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">This is the descrption </p>
+      <div className="flex ml-6 mt-10 flex-col">
+        <h4 className="font-bold text-[20px] hover:underline sm:text-[16px]">
+          Venkat
+        </h4>
+        <span className="text-sm text-gray-500 sm:text-[15px]">
+          @venkat1234
+        </span>
+        <p className="text-gray-800 text-[15px sm:text-[16px] mb-2 mt-2">
+          This is the descrption This is the descrption This is the descrption
+          This is the descrption This is the descrption This is the descrption
+          This is the descrption This is the descrption
+        </p>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center p-3">
         <div className="flex items-center">
-        <LocationMarkerIcon className="h-10 w-10  p-2 text-gray-400"/>
-        <span className=" text-lg p-2 ">Hindupur</span>
+          <LocationMarkerIcon className="h-10 w-10  p-2 text-gray-400" />
+          <span className=" text-lg p-2 ">Hindupur</span>
         </div>
         <div className="flex items-center">
-        <CakeIcon className="h-10 w-10  p-2 text-gray-400"/>
-        <span className=" text-lg p-2 ">Hindupur</span>
+          <CakeIcon className="h-10 w-10  p-2 text-gray-400" />
+          <span className=" text-lg p-2 ">Hindupur</span>
         </div>
         <div className="flex items-center">
-        <CalendarIcon className="h-10 w-10  p-2 text-gray-400"/>
-        <span className=" text-lg p-2 ">Hindupur</span>
+          <CalendarIcon className="h-10 w-10  p-2 text-gray-400" />
+          <span className=" text-lg p-2 ">Hindupur</span>
         </div>
       </div>
+      <EditForm />
     </div>
   );
 }
