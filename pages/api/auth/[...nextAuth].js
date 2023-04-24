b6d4371a79cfdf9c5307126ef590a56c7b8ceac2
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-24 14:58:29 +0530

    Modified [...nextauth].js

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -15,14 +15,16 @@ export const authOptions = {
   },
   secret: process.env.SECRET,
 
-  callbacks:{
-    async session({session,token}){
-      session.user.uid = token.sub
-      session.user.username = session.user.email.split('@')[0]
+  callbacks: {
+    async session({ session, token }) {
+      session.user.username = session.user.name
+        .split(" ")
+        .join("")
+        .toLocaleLowerCase();
+      session.user.uid = token.sub;
       return session;
-    }
-
-  }
+    },
+  },
 }
 
 
