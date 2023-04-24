dc250ce034373be5b3ec48cd4c70fee7bf05ab7f
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-24 14:52:47 +0530

    Modified hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default function handler(request, response) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
 }
