a9bfa287186432f8cca887d21888968f6d5de1f1
venkatsaireddy1234 <venkatsaireddy1234@gmail.com>
2023-04-25 12:43:05 +0530

    [...nextauth].js changed

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -1,7 +1,40 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google"
 
-export const authOptions = {
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+//   pages:{
+//     signIn: '/auth/signin'
+//   },
+
+
+//   secret: process.env.SECRET,
+
+//   callbacks: {
+//     async session({ session, token }) {
+//       session.user.username = session.user.name
+//         .split(" ")
+//         .join("")
+//         .toLocaleLowerCase();
+//       session.user.uid = token.sub;
+//       return session;
+//     },
+//   },
+// }
+
+
+// export default NextAuth(authOptions)
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
@@ -10,10 +43,10 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  pages:{
-    signIn: '/auth/signin'
-  },
 
+  pages: {
+    signIn: "/auth/signin",
+  },
 
   secret: process.env.SECRET,
 
@@ -27,7 +60,4 @@ export const authOptions = {
       return session;
     },
   },
-}
-
-
-export default NextAuth(authOptions)
+});
